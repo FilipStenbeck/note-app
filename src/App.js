05440b69540be66030c3c77fb0c6631273e9e3c1
Filip Stenbeck <filip.stenbeck@gmail.com>
2019-11-06 18:20:33 +0100

    adds header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Main from './Main';
+import Header from './Header';
 
 export default function App() {
     return (
-        <Container maxWidth="sm">
+        <Container maxWidth="l">
+            <Header />
             <Box my={4}>
                 <Typography variant="h4" component="h1" gutterBottom>
                     Note taking app
