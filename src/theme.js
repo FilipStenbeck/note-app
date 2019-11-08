3af381308b7c505509788bf8079df8d8ab70392f
Filip Stenbeck <filip.stenbeck@gmail.com>
2019-11-08 15:50:39 +0100

    changing some colors

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,21 @@
-import { red } from "@material-ui/core/colors";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#556cd6"
-    },
-    secondary: {
-      main: "#19857b"
-    },
-    error: {
-      main: red.A400
-    },
-    background: {
-      default: "#fff"
+    palette: {
+        primary: {
+            main: '#0b8551'
+        },
+        secondary: {
+            main: '#19857b'
+        },
+        error: {
+            main: red.A400
+        },
+        background: {
+            default: '#ebf7f6'
+        }
     }
-  }
 });
 
 export default theme;
